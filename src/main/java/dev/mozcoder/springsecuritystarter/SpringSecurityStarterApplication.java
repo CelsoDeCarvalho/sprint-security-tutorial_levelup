61977cfba514169f6b9abe9ba7b7ef91597c3fed
Celso Octaviano De Carvalho <celso@celso>
2022-08-11 10:45:04 +0200

    init commit

---
+++ src/main/java/dev/mozcoder/springsecuritystarter/SpringSecurityStarterApplication.java
@@ -0,0 +1,13 @@
+package dev.mozcoder.springsecuritystarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mozcoder/springsecuritystarter/SpringSecurityStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mozcoder.springsecuritystarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
