9b3d9f921abec9af8f07e0aeffdc120c80c3ebcc
Celso De Carvalho <celsommade361@gmail.com>
2022-11-13 08:42:36 +0200

    create in memory authentication configurations and controller for authentication succefully message

--- src/main/java/dev/mozcoder/springsecuritystarter/security/SecurityConfig.java
+++ src/main/java/dev/mozcoder/springsecuritystarter/security/SecurityConfig.java
@@ -1,4 +1,27 @@
 package dev.mozcoder.springsecuritystarter.security;
 
-public class SecurityConfig{
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("Celso")
+                .password(passwordEncoder().encode("celso1999"))
+                .roles("user");
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
